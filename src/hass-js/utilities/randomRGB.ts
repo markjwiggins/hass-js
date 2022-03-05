89004255bb7ef34cbcafd95a5d9ec123bbccc5b0
Mark Wiggins <markjwiggins@gmail.com>
2022-03-05 19:42:25 +0000

    Random Helper utilities

---
+++ src/hass-js/utilities/randomRGB.ts
@@ -0,0 +1,5 @@
+import randomIntFromInterval from "./randomIntFromInterval";
+
+const randomRGB = () => [randomIntFromInterval(0, 255), randomIntFromInterval(0, 255), randomIntFromInterval(0, 255)];
+
+export default randomRGB;
